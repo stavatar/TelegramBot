624b09cbe7aba572510121280134e44fa6ddce72
stavatar <stavatar@yandex.ru>
2022-03-13 22:52:59 +0300

    Костыль

--- src/test/java/com/example/TelegramBot/TelegramBotApplicationTests.java
+++ src/test/java/com/example/TelegramBot/TelegramBotApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.TelegramBot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class TelegramBotApplicationTests {
 
 	@Test
 	void contextLoads() {
+		SpringApplication.run(TelegramBotApplication.class);
 	}
 
 }
